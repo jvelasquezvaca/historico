051d0be16c73ee7e174415fb4695abfbff3f67ff
jvelasquezvaca <13j.vel@gmail.com>
2023-09-01 11:57:47 -0500

    Primer Commit

---
+++ controllers/historico.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * historico controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::historico.historico');
---
+++ routes/historico.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * historico router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::historico.historico');
